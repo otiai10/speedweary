aa558e9c72c1f41f18dad3d4e959f7c2f69a02e1
yuta-amano <yuta.amano@gree.net>
2013-07-19 09:39:25 +0900

    add triggers

--- speedweary/ShapViewController.m
+++ speedweary/ShapViewController.m
@@ -9,9 +9,13 @@
 #import "ShapViewController.h"
 
 @interface ShapViewController ()
--(void)displayKao;
+-(void)displayFace;
 -(void)hideStartButton;
 -(void)showStartButton;
+-(void)onFaceDead;
+-(void)hideFace;
+-(void)displayChoices;
+-(void)hideChoices;
 @end
 
 @implementation ShapViewController
@@ -21,6 +25,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.kaomojilist = [[NSArray alloc] initWithObjects:@"ヽ(ﾟ∀｡)ﾉｳｪー", @"( ﾟДﾟ)", @"（☝ ՞ਊ ՞）☝",nil];
+    [self onChoiceTapped];
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,25 +33,67 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)startGame:(UIButton *)sender {
-    [self displayKao];
+
+// trigger to scene 2
+- (IBAction)onStartButtonPressed:(UIButton *)sender {
+    [self displayFace];
     [self hideStartButton];
+    // pseudo timer
+    // sync sleep
+
+    [self hideFace];
+    [self onFaceDead];
+}
+
+- (IBAction)onChoice01Tapped:(UIButton *)sender {
+    [self onChoiceTapped];
+}
+
+- (IBAction)onChoice02Tapped:(UIButton *)sender {
+    [self onChoiceTapped];
+}
+
+// trigger to scene 3
+- (void)onFaceDead {
+    NSLog(@"onFaceDead");
+    // display choices
+    [self displayChoices];
+}
+
+// trigger to scene 1
+- (void)onChoiceTapped {
+    [self hideChoices];
+    NSLog(@"oppai!");
     [self showStartButton];
 }
 
-- (void)displayKao {
-    NSLog (@"displayKao");
+- (void)displayFace {
+    NSLog (@"displayFace");
     int i = random() % self.kaomojilist.count;
     self.target.text = self.kaomojilist[i];
 }
 
+- (void)hideFace {
+    self.target.hidden = YES;
+}
+
 - (void)hideStartButton {
-    NSLog (@"hide");
+    NSLog (@"hideStartButton");
     self.startButton.hidden = YES;
 }
 - (void)showStartButton {
-    NSLog (@"show");
+    NSLog (@"showStartButton");
     self.startButton.hidden = NO;
+}
+
+- (void)displayChoices {
+    self.choice01.hidden = NO;
+    self.choice02.hidden = NO;
+}
+
+- (void)hideChoices {
+    self.choice01.hidden = YES;
+    self.choice02.hidden = YES;
 }
 
 @end
